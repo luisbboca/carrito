925241c90af5a62d35db3fe721cb3a0078f9475d
luisbboca <107447827+luisbboca@users.noreply.github.com>
2022-09-06 00:28:45 -0300

    Update scripts.js

--- carrito/js/scripts.js
+++ carrito/js/scripts.js
@@ -54,18 +54,19 @@ function addItemCarrito(newItem){
 function renderCarrito(){
   tbody.innerHTML = ''
   carrito.map(item => {
+    let{img,title,precio,cantidad} = item
     const tr = document.createElement('tr')
     tr.classList.add('ItemCarrito')
     const Content = `
     
     <th scope="row">1</th>
             <td class="table__productos">
-              <img src=${item.img}  alt="">
-              <h6 class="title">${item.title}</h6>
+              <img src=${img}  alt="">
+              <h6 class="title">${title}</h6>
             </td>
-            <td class="table__price"><p>${item.precio}</p></td>
+            <td class="table__price"><p>${precio}</p></td>
             <td class="table__cantidad">
-              <input type="number" min="1" value=${item.cantidad} class="input__elemento">
+              <input type="number" min="1" value=${cantidad} class="input__elemento">
               <button class="delete btn btn-danger">x</button>
             </td>
     
@@ -131,9 +132,9 @@ function addLocalStorage(){
 }
 
 window.onload = function(){
-  const storage = JSON.parse(localStorage.getItem('carrito'));
-  if(storage){
+  const storage = JSON.parse(localStorage.getItem('carrito')) ||[]
+
     carrito = storage;
     renderCarrito()
-  }
+  
 }
